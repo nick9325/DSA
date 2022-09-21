a4f630ea8bd912c1d4fdfe4b62e26ed6754fb4d8
Nikhil <nikhildmagar@gmail.com>
2022-09-21 19:27:46 +0530

    min_path_sum_triangle

---
+++ Dynamic Programming/min_path_sum_in_triangle.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h> 
+int f(vector<vector<int>>& triangle,int n, int i,int j,vector<vector<int>> dp){
+    if(i==n-1) return triangle[i][j];
+    if(dp[i][j]!=-1) return dp[i][j];
+    int down=f(triangle,n,i+1,j,dp)+triangle[i][j];
+    int diag=f(triangle,n,i+1,j+1,dp)+triangle[i][j];
+    
+    return dp[i][j]=min(down,diag);
+}
+
+int f2(vector<vector<int>>& triangle, int n,vector<vector<int>> dp){
+    for(int i=0;i<n;i++){
+        dp[n-1][i]=triangle[n-1][i];
+    }
+    for(int i=n-2;i>=0;i--){
+        for(int j=i;j>=0;j--){
+            
+            dp[i][j]=min(triangle[i][j]+dp[i+1][j],triangle[i][j]+dp[i+1][j+1]);
+        }
+    }
+    return dp[0][0];
+}
+
+int minimumPathSum(vector<vector<int>>& triangle, int n){
+	// Write your code here.
+    vector<vector<int>> dp(n,vector<int>(n,-1));
+    
+//     return f(triangle,n,0,0,dp);
+    
+    return f2(triangle,n,dp);
+}
