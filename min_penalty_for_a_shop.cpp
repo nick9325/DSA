892bd36896f790445334fe401eba3f1ceb790d75
Nikhil <nikhildmagar@gmail.com>
2022-11-28 19:19:43 +0530

    min_penalty

---
+++ min_penalty_for_a_shop.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int bestClosingTime(string c) {
+        
+        int cnty=0;
+        int cntn=0;
+        
+        for(int i=0;i<c.length();i++){
+            if(c[i]=='Y'){
+                cnty++;
+            }
+            else{
+                cntn++;
+            }
+        }
+        if(cnty==0) return 0;
+        if(cntn==0) return c.length();
+        if(c.length()==1 and c[0]=='Y') return 1;
+        if(c.length()==1 and c[0]=='N') return 0;
+        int res=cnty,ans=0,cc=cnty;
+        for(int i=1;i<=c.length();i++){
+            
+            if(c[i-1]=='Y'){
+                res--;
+            }
+            else{
+                res++;
+            }
+            if(res<cc){
+                ans=i;
+                cc=res;
+            }
+        }
+        
+        return ans;
+    }
+};
