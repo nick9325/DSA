f820dcae2d1616d72152b1373f7e9ebd4acc9b48
Nikhil <nikhildmagar@gmail.com>
2022-12-19 22:28:12 +0530

    create delete_leaves_with_given_value

---
+++ Trees/delete_leaves_with_given_value.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    TreeNode* removeLeafNodes(TreeNode* root, int target) {
+        if(root==NULL) return NULL;
+
+        root->left=removeLeafNodes(root->left,target);
+        root->right=removeLeafNodes(root->right,target);
+
+        if(root->left==NULL and root->right==NULL and root->val==target){
+            return NULL;
+        }
+
+        return root;
+    }
+
+};
