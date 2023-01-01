c396749dd1f818d41c5f89dc53f7f8239d2f39d7
Nikhil <nikhildmagar@gmail.com>
2023-01-01 22:02:57 +0530

    create word_pattern

---
+++ Others(Greedy,Stack,Queue etc.)/word_pattern.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) {
+        if(pattern.size()==1 and s.size()==1) return true;
+        if(pattern==s) return false;
+        unordered_map<string,char> mp;  
+        unordered_map<char,int> m;    
+        int k=0;
+        string word="";
+        for(int i=0;i<s.length();i++){
+            while(s[i]!=' ' and i<s.length()){
+                word+=s[i];
+                i++;
+            }
+            if(s[i]==' '){
+                if(mp.find(word)==mp.end()){
+                    if(m.find(pattern[k])!=m.end()) return false;
+                    mp[word]=pattern[k];
+                    m[pattern[k]]++;
+                }
+                k++;
+                word="";
+            }
+        }
+        if(mp.find(word)==mp.end()){
+            if(m.find(pattern[k])!=m.end()) return false;
+            mp[word]=pattern[k];
+        }
+        k++;
+        if(k!=pattern.size()) return false;
+        word="";
+        k=0;
+
+        for(int i=0;i<s.length();i++){
+            while(s[i]!=' ' and i<s.length()){
+                word+=s[i];
+                i++;
+            }
+            if(s[i]==' '){
+                if(mp.find(word)==mp.end() or mp[word]!=pattern[k]) return false;
+                word="";
+                k++;
+            }
+        }
+        if(mp.find(word)==mp.end() or mp[word]!=pattern[k]) return false;
+
+        return true;
+        
+    }
+};
