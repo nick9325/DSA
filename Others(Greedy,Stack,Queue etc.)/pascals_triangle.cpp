a82549ab949c2f55c827975c4b5926a9b53cc695
Nikhil <nikhildmagar@gmail.com>
2023-06-06 04:35:47 +0530

    pascals_triangle

---
+++ Others(Greedy,Stack,Queue etc.)/pascals_triangle.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        
+      //  Approach 1
+
+        vector<vector<int>> ans;
+        ans.push_back({1});
+        int i=0;
+        vector<int> temp;
+        temp.push_back({1});
+        
+        while(i<numRows-1){
+            vector<int> res;
+            res.push_back({1});
+            int j=1;
+            while(j<temp.size()){
+                res.push_back({temp[j-1]+temp[j]});
+                j++;
+            }
+
+            res.push_back({1});
+            ans.push_back(res);
+
+            temp=res;
+            i++;
+    }
+
+    return ans;
+
+    //Approach 2
+
+
+    vector<vector<int>> ans;
+
+
+    //Current element = prevElement * (rowNumber - colIndex) / colIndex
+
+    for(int i=1;i<=numRows;i++){
+
+        vector<int> res;
+        res.push_back(1);
+        int cur=1;
+
+        for(int j=1;j<i;j++){
+            res.push_back((cur*(i-j))/j);
+            cur=(cur*(i-j))/j;
+        }
+
+        ans.push_back(res);
+
+    }
+    return ans;
+
+
+    }
+};
