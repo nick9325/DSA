96a799dbaaa065b2eba91012a6dd169485dc93a0
Nikhil <nikhildmagar@gmail.com>
2023-06-06 04:46:19 +0530

    pascals_triangle

--- Others(Greedy,Stack,Queue etc.)/pascals_triangle.cpp
+++ Others(Greedy,Stack,Queue etc.)/pascals_triangle.cpp
@@ -53,5 +53,20 @@ public:
     return ans;
 
 
+
+    
+    //Approach 3
+
+    vector<vector<int>> ans;
+
+    for(int i=0;i<numRows;i++){
+        vector<int> res(i+1,1);
+        for(int j=1;j<i;j++){
+            res[j]=ans[i-1][j-1]+ans[i-1][j];
+        }
+        ans.push_back(res);
+    }
+
+
     }
 };
